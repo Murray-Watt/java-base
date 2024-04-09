75f1a5d59e5c3256549961ce775e6aeff4cec90a
murray-watt <murray-watt@hotmail.com>
2024-04-08 23:01:46 -0600

    Fixed Project Structure Put a demo of instance and static fields in a class
    Co-authored-by: Ian Watt

--- base/src/main/java/org/mwatt/App.java
+++ base/src/main/java/org/mwatt/App.java
@@ -1,39 +1,14 @@
 package org.mwatt;
 
+import lombok.Getter;
+
+@Getter
 public class App
 {
-    private int instanceInt = 0;
-    private static int staticInt = 0;
-    private static long staticLong = 0;
-
-    public int getInstanceInt() {
-        return instanceInt;
-    }
+    private int instanceInt;
 
     public int incrementInstanceInt() {
         return ++instanceInt;
-    }
-
-    /*
-     * Normally you would not have a getter for a static variable, but this is just for demonstration purposes.
-     */
-    public int getStaticInt() {
-        return staticInt;
-    }
-
-    /*
-        * Normally you would not have a setter for a static variable, but this is just for demonstration purposes.
-     */
-    public int incrementStaticInt() {
-        return ++staticInt;
-    }
-
-    public static long getStaticLong() {
-        return staticLong;
-    }
-
-    public static long incrementStaticLong() {
-        return ++staticLong;
     }
 
     public static void main( String[] args )
